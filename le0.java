500f25dd68ed9e5536a6f8b4719977fca0d5ce43
221B413 <221B413@juet.co.in>
2025-01-16 12:06:30 +0530

    this is my 1st exercise

---
+++ le0.java
@@ -0,0 +1,48 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+// Java Program to Illustrate Getter and Setter
+
+// Importing input output classes
+import java.io.*;
+
+// Class 1
+// Helper class
+class GetSet {
+
+	// Member variable of this class
+	private String name;
+
+	// Method 1 - Getter
+	public String getName() { return name; }
+
+	// Method 2 - Setter
+	public void setName(String N)
+	{
+
+		// This keyword refers to current instance itself
+		this.name = N;
+	}
+}
+
+// Class 2
+// Main class
+class GFG {
+
+	// Main driver method
+	public static void main(String[] args)
+	{
+		// Creating an object of class 1 in main() method
+		GetSet obj = new GetSet();
+
+		// Setting the name by calling setter method
+		obj.setName("Geeks for Geeks");
+		// Getting the name by calling getter method
+		System.out.println(obj.getName());
+	}
+}
